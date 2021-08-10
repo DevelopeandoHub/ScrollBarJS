45fa9ec22aba4f98b81f1ab7fd724eae272779b2
SantiagoSinatra <santiagosinatra@gmail.com>
2021-08-10 18:26:45 -0300

    commit final

--- js/progress-bar.js
+++ js/progress-bar.js
@@ -1,7 +1,7 @@
+/* Apenas se carga el sitio, nuestro codigo le hace una petición al buscador para que actualice la barra de progreso cada vez que redibuja la pantalla */
+
 window.addEventListener('load', () => {
     const progress = document.getElementById('progress');
-    /* console.log(progress); */
-    /* update(); */
     requestAnimationFrame(update);
     
   });
